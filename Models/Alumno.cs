ae8bfac96dac1666de0be43157b0a3a4ce836e10
Estiven Yepes Arenas <estivenyepes@gmail.com>
2025-10-07 16:34:22 -0500

    POO in C#

---
+++ Models/Alumno.cs
@@ -0,0 +1,64 @@
+using System.Runtime.CompilerServices;
+
+namespace POO.Models
+{
+    public class Alumno
+    {
+        private int _id;
+        private string _nombre = string.Empty;
+        private string _apellido = string.Empty;
+
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("El id no puede ser negativo");
+                }
+                _id = value;
+            }
+        }
+        public string Nombre
+        {
+            get { return _nombre; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("El nombre no puede ser vacio");
+                }
+                _nombre = value;
+            }
+        }
+
+        public string Apellido
+        {
+            get { return _apellido; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("El apellido no puede ser vacio");
+                }
+                _apellido = value;
+            }
+        }
+
+        public Alumno() { }
+        public Alumno(int id, string nombre, string apellido)
+        {
+            Id = id;
+            Nombre = nombre;
+            Apellido = apellido;
+        }
+
+
+        public static void MostrarNombre()
+        {
+            Console.Write("Hola soy un Alumno y se decir mi nombre");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using POO.Models;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Alumno alumno = new Alumno();
+        Alumno alu1 = new Alumno(32, "Estiven", "Yepes");
+        Console.WriteLine(alu1.Nombre);
+        Console.WriteLine(alu1.Apellido);
+        Console.WriteLine(alu1.Id);
+       
+
+    }
+ }
